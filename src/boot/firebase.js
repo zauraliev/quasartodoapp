b72c2d58103ea0a4d1908e877accf277ed7ae152
Zaur <zaur_aliev@hotmail.com>
2021-03-27 17:03:53 -0400

    Testing production deployment with firebase config

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -5,13 +5,14 @@ import "firebase/database"
 
 // ADD YOUR FIREBASE CONFIG OBJECT HERE:
 var firebaseConfig = {
-  apiKey: "YOURAPIKEY",
-  authDomain: "YOURAUTHDOMAIN",
-  databaseURL: "YOURDATABASEURL",
-  projectId: "YOURPROJECTID",
-  storageBucket: "YOURSTORAGEBUCKET",
-  messagingSenderId: "YOURMESSAGINGSENDERID",
-  appId: "YOURAPPID"
+  apiKey: "AIzaSyAprerqxFAC_f72AdrWFmMO7BpyjrcCqpg",
+  authDomain: "quasar-todo-app.firebaseapp.com",
+  databaseURL: "https://quasar-todo-app.firebaseio.com",
+  projectId: "quasar-todo-app",
+  storageBucket: "",
+  messagingSenderId: "688094371877",
+  appId: "1:688094371877:web:fa2415135525fc0f691fcc",
+  measurementId: "G-8LGF4BMTY9"
 }
 // Initialize Firebase
 let firebaseApp = firebase.initializeApp(firebaseConfig)
